1fa5c346d29a5d017c8a8ba635fadd6540d2c0e4
Sasha <sasha982025@gmail.com>
2019-08-27 00:45:45 +0300

    rezult

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import useWhyDidYouUpdate from './UseWhyDidYouUpdate';
+import'./App.css'
+
+const Counter = React.memo(props => {
+  useWhyDidYouUpdate('Counter', props);
+  return <div className="counter">{props.count}</div>;
+});
+
 
 function App() {
+  const [count, setCount] = useState(0);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className='container'>
+        <Counter count={count} />
+        <button onClick={() => setCount(count + 1)}>Increment</button>
+        <button onClick={() => setCount(count - 1)}>Decrement </button>       
+      </div>
   );
 }
 
