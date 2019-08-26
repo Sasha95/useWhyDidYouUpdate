3778f0f3078daec1aef80c2d6752279e5cd9de41
Sasha <sasha982025@gmail.com>
2019-08-27 00:49:12 +0300

    Updates

---
+++ precache-manifest.81496f41ed829d5d5ddfb289bebbeae9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9aa92ed27ae79ebd4f49467d30bdda20",
+    "url": "/useWhyDidYouUpdate/index.html"
+  },
+  {
+    "revision": "91351f832360a180f429",
+    "url": "/useWhyDidYouUpdate/static/css/main.e1a244c8.chunk.css"
+  },
+  {
+    "revision": "0d349b2ec1f29fabda1e",
+    "url": "/useWhyDidYouUpdate/static/js/2.8f8897bf.chunk.js"
+  },
+  {
+    "revision": "91351f832360a180f429",
+    "url": "/useWhyDidYouUpdate/static/js/main.8709240f.chunk.js"
+  },
+  {
+    "revision": "494416fa4fc1274098a0",
+    "url": "/useWhyDidYouUpdate/static/js/runtime~main.a368e7d5.js"
+  }
+]);
